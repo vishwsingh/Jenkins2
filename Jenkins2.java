ea0cf65c12ffe8837bf40d5dc385b2b1a617eb28
vishwsingh <118203821+vishwsingh@users.noreply.github.com>
2023-03-21 11:22:47 -0400

    Create Jenkins2.java

---
+++ Jenkins2.java
@@ -0,0 +1,9 @@
+public class Jenkins2 {
+  public static void main(String[] args) {
+      for(int i = 0;i<10;i++) {
+          for(int j = i+1;j<10;j++) {
+              System.out.println("*");
+          }
+      }
+  }
+}
